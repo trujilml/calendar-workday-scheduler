f8e07a1d19ef4e88af07573f2a561e14fc236945
trujilml <mickeytrj@gmail.com>
2021-10-15 16:23:47 -0500

    created js file, figuring out bare elements for calendar format with bootstrap, updated readme file

---
+++ Develop/script.js
@@ -0,0 +1,4 @@
+// figure out how to get date and time and dat in title header
+//continue html calendar format - figure out how to have all time the same without repeating multiple ids
+
+moment(date).format('MMMM Do YYYY, h:mm:ss a');
