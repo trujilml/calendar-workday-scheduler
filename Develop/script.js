fdc9b7ce20ab494ff2e56dc7d1bfff5b08876451
trujilml <mickeytrj@gmail.com>
2021-10-16 16:03:34 -0500

    added jquery bootstrap popper and file links to implement working time

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // figure out how to get date and time and dat in title header
 //continue html calendar format - figure out how to have all time the same without repeating multiple ids
 
-moment(date).format('MMMM Do YYYY, h:mm:ss a');
+
+var currentDayEl = document.querySelector("#currentDay");
+var today = moment();
+
+currentDayEl.textContent = today.format("dddd, MMMM Do YYYY, h:mm a");
