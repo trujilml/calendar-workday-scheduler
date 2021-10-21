57ff72337bc66244f17ecfd78430f0b4d76ebf04
trujilml <mickeytrj@gmail.com>
2021-10-21 11:57:41 -0500

    finally got save function to work, readjusted names of time blocks, removed programs not used, used moment js for now

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
-    $(".save-Btn").on("click",function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
+
+    $(".saveBtn").on("click",function() {
     var savedTaskToDo = $(this).siblings(".description").val();
     var timeDue = $(this).parent().attr("id");
 
-
     localStorage.setItem(timeDue, savedTaskToDo);
-});
+})
+
+$("#timehour-8 .description").val(localStorage.getItem("timehour-8"));
+$("#timehour-9 .description").val(localStorage.getItem("timehour-9"));
+$("#timehour-10 .description").val(localStorage.getItem("timehour-10"));
+$("#timehour-11 .description").val(localStorage.getItem("timehour-11"));
+$("#timehour-12 .description").val(localStorage.getItem("timehour-12"));
+$("#timehour-13 .description").val(localStorage.getItem("timehour-13"));
+$("#timehour-14 .description").val(localStorage.getItem("timehour-14"));
+$("#timehour-15 .description").val(localStorage.getItem("timehour-15"));
+$("#timehour-16 .description").val(localStorage.getItem("timehour-16"));
+$("#timehour-17 .description").val(localStorage.getItem("timehour-17"));
 
 
 function getCurrentHour() {
-    var currentHour = moment().hours();
+    var currentHour = moment().hour();
 
-    $(".time-block").each(function() {
-        var hourOfWorkTime = parseInt($(this).attr("id").split("-")[1]);
+    $(".time-block").each(function () {
+        var hourOfWorkTime = parseInt($(this).attr("id").split("timehour-")[1]);
 
         if (hourOfWorkTime < currentHour) {
             $(this).addClass("past");
@@ -26,23 +38,14 @@ function getCurrentHour() {
         $(this).removeClass("present");
         $(this).addClass("future");
        }
-     });
+     })
     }
+
 getCurrentHour();
 
-var currentDayEl = document.querySelector("#currentDay"); 
-var today = moment();
-currentDayEl.textContent = today.format("dddd, MMMM Do YYYY, h:mm a");
-
-
-
-var interval = setInterval(getCurrentHour, 15000);
-
-$("#amhour-8 .description").val(localStorage.getItem("amhour-8"));
-
-
-
-
-$("#pmhour-12 .description").val(localStorage.getItem("pmhour-12"));
-
 })
+
+
+
+
+
