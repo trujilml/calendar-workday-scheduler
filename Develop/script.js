5e70e672c324c1313de15416cb86e7bb05426ae2
trujilml <mickeytrj@gmail.com>
2021-10-21 11:17:37 -0500

    readjusted css and js files, added save btn function and function to obtain current hour from moment

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,48 @@
-// figure out how to get date and time and dat in title header
-//continue html calendar format - figure out how to have all time the same without repeating multiple ids
+$(document).ready(function() {
+    $(".save-Btn").on("click",function() {
+    var savedTaskToDo = $(this).siblings(".description").val();
+    var timeDue = $(this).parent().attr("id");
 
+
+    localStorage.setItem(timeDue, savedTaskToDo);
+});
+
+
+function getCurrentHour() {
+    var currentHour = moment().hours();
+
+    $(".time-block").each(function() {
+        var hourOfWorkTime = parseInt($(this).attr("id").split("-")[1]);
+
+        if (hourOfWorkTime < currentHour) {
+            $(this).addClass("past");
+        }
+       else if (hourOfWorkTime === currentHour) {
+           $(this).removeClass("past");
+           $(this).addClass("present");
+       }
+       else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+       }
+     });
+    }
+getCurrentHour();
 
 var currentDayEl = document.querySelector("#currentDay"); 
 var today = moment();
-
 currentDayEl.textContent = today.format("dddd, MMMM Do YYYY, h:mm a");
 
+
+
+var interval = setInterval(getCurrentHour, 15000);
+
+$("#amhour-8 .description").val(localStorage.getItem("amhour-8"));
+
+
+
+
+$("#pmhour-12 .description").val(localStorage.getItem("pmhour-12"));
+
+})
